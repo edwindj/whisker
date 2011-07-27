9c2f939b065371d06b2dd02c37d79675a57c7539
Edwin de Jonge <edwindjonge@gmail.com>
2011-07-27 17:35:51 +0200

    added NAMESPACE

--- R/whisker.R
+++ R/whisker.R
@@ -1,6 +1,28 @@
-#' whisker
+#' Whisker
 #'
 #' This is whisker
-whisker <- function(){
+#' @param template \code{character}
+#' @param data named \code{list} 
+#' @export
+whisker <- function(template, data){
+   parse(template)
+}
+
+parse <- function(template){
+  delim <- strsplit("{{ }}"," ")[[1]]
   
+  buildre <- function(delim){
+     re <- paste(delim[1],"\\s*(\\S+)\\s*", delim[2], sep="")
+     re <- gsub("([{<>}])", "\\\\\\1", re)
+     re
+  }
+  re <- buildre(delim)
+  
+  rex <- regexpr(re, template)
+
+  print(list( delim=delim
+  #          , re = re
+            , rex=rex
+            )
+       )
 }
